74d2779a0420b8de112ed35860a7d4c317c936ed
Amirali Esmaeili <esmailiamirali@gmail.com>
2023-06-25 14:27:42 +0330

    Init

---
+++ build.sbt
@@ -0,0 +1,5 @@
+organization := "io.siever"
+
+name := "Java playground"
+
+version := "1.0-SNAPSHOT"
---
+++ src/main/java/io/siever/Main.java
@@ -0,0 +1,8 @@
+package io.siever;
+
+public class Main {
+  public static void main(String[] args) {
+	System.out.println("hello world");
+  }
+}
+
